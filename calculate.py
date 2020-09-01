7b65480201b89b485ab1a63d027a246293431f73
Jeremy Brown <48503545+jrb07@users.noreply.github.com>
2020-09-01 10:52:02 -0600

    Added print output that explained what is going on during the execution of "calculate..py" by "run.sh"

--- calculate.py
+++ calculate.py
@@ -1,6 +1,8 @@
 import math_lib
 
+print('Addition of 1+2 = ')
 print(math_lib.add(1, 2))
+print('Division of 1/2 = ')
 print(math_lib.div(1, 2))
 #Added input so that the shell executable window will not close 
 # too quickly to see the output.
